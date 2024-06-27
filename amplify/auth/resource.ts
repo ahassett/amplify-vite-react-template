eecc8a2b376b4a179c5cba55837f481d3a747fc7
ahass <a.hassett@outlook.com>
2024-06-27 12:41:54 -0400

    🔧 new config - removed amplify authenticator and replaced with cognito user pools

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,6 +1,6 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { defineAuth } from "@aws-amplify/backend";
 
-/**
+/** USING AMPLIFY AUTHENTICATOR
  * Define and configure your auth resource
  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
  */
--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,37 @@
-import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
-import { data } from './data/resource';
+// WITH AMPLIFY AUTHENTICATOR
+// import { defineBackend } from '@aws-amplify/backend';
+// import { auth } from './auth/resource';
+// import { data } from './data/resource';
 
-defineBackend({
-  auth,
-  data,
+// defineBackend({
+//   auth,
+//   data,
+// });
+
+// WITH COGNITO AUTHENTICATOR
+import { defineBackend } from "@aws-amplify/backend";
+
+/**
+ * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
+ */
+const backend = defineBackend({});
+
+backend.addOutput({
+  auth: {
+    aws_region: "us-east-2",
+    user_pool_id: "us-east-2_0xJbTwpbz",
+    user_pool_client_id: "44tnt619h9pc9d12habgh3srpb",
+    //identity_pool_id: "<your-cognito-identity-pool-id>",
+    //username_attributes: ["email"],
+    // standard_required_attributes: ["email"],
+    // user_verification_types: ["email"],
+    // unauthenticated_identities_enabled: true,
+    // password_policy: {
+    //   min_length: 8,
+    //   require_lowercase: true,
+    //   require_uppercase: true,
+    //   require_numbers: true,
+    //   require_symbols: true,
+    // }
+  },
 });
